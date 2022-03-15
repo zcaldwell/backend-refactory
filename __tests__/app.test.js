74b35d3b9560c94464205676ffed82d2317cba35
zcaldwell <mintyst.john@gmail.com>
2022-03-14 17:17:40 -0700

    yeah..

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,27 +4,6 @@ const request = require('supertest');
 const app = require('../lib/app');
 const Order = require('../lib/models/Order');
 
-// TODO: Remove this function & use the Order model
-async function createOrder({ product, quantity }) {
-  const { rows } = await pool.query(
-    'INSERT INTO orders(product, quantity) VALUES ($1, $2) RETURNING *;',
-    [product, quantity]
-  );
-  return new Order(rows[0]);
-}
-
-// TODO: Remove this function & use the Order model
-async function getOrderById(id) {
-  const { rows } = await pool.query(
-    'SELECT * FROM orders WHERE id=$1;',
-    [id]
-  );
-
-  if (!rows[0]) return null;
-
-  return new Order(rows[0]);
-}
-
 describe('refactory routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -47,14 +26,14 @@ describe('refactory routes', () => {
   });
 
   it('should be able to list an order by id', async () => {
-    const order = await createOrder({ product: 'Widget', quantity: 1 });
+    const order = await Order.insert({ product: 'Widget', quantity: 1 });
     const res = await request(app).get(`/api/v1/orders/${order.id}`);
 
     expect(res.body).toEqual(order);
   });
 
   it('should be able to list orders', async () => {
-    await createOrder({ product: 'Widget', quantity: 1 });
+    await Order.insert({ product: 'Widget', quantity: 1 });
     const res = await request(app).get('/api/v1/orders');
 
     expect(res.body).toEqual([
@@ -67,7 +46,7 @@ describe('refactory routes', () => {
   });
 
   it('should be able to update an order', async () => {
-    const order = await createOrder({ product: 'Widget', quantity: 1 });
+    const order = await Order.insert({ product: 'Widget', quantity: 1 });
     const res = await request(app)
       .patch(`/api/v1/orders/${order.id}`)
       .send({ product: 'Thingamajig', quantity: 2 });
@@ -79,14 +58,14 @@ describe('refactory routes', () => {
     };
 
     expect(res.body).toEqual(expected);
-    expect(await getOrderById(order.id)).toEqual(expected);
+    expect(await Order.getById(order.id)).toEqual(expected);
   });
 
   it('should be able to delete an order', async () => {
-    const order = await createOrder({ product: 'Widget', quantity: 1 });
+    const order = await Order.getById({ product: 'Widget', quantity: 1 });
     const res = await request(app).delete(`/api/v1/orders/${order.id}`);
 
     expect(res.body).toEqual(order);
-    expect(await getOrderById(order.id)).toBeNull();
+    expect(await Order.getById(order.id)).toBeNull();
   });
 });
